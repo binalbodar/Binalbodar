eb1fa289f705cdd667c24014ae4578b8ee82b627
binalbodar <binalbodar23@gmail.com>
2022-04-29 18:53:36 +0530

    29_4_22

--- src/App.js
+++ src/App.js
@@ -103,9 +103,70 @@ function App() {
 
   //isArray
   // let arr=10;
-  let arr=[10,"Binal",20,30,40,50];
-  let res=Array.isArray(arr);
-  console.log(res);
+  //let arr=[10,"Binal",20,30,40,50];
+  //let res=Array.isArray(arr);
+  //console.log(res);
+
+  //map
+  // let array =[
+  //   {id:101, name:"abacavir", quantity:25, price:150, expiry:2022, status:true},
+  //   {id:102, name:"Eltrombopag", quantity:90, price:550, expiry:2021, status:true},
+  //   {id:103, name:"Meloxicam", quantity:85, price:450, expiry:2025, status:false},
+  //   {id:104, name:"Allopurinol", quantity:50, price:600, expiry:2023, status:true},
+  //   {id:105, name:"Phenytoin", quantity:63, price:250, expiry:2021, status:false}
+  // ];
+  // let map=array.map(x => console.log(x.id, x.name, x.quantity, x.price, x.expiry, x.status));
+
+  //filter
+  // function xdate(expiry){
+  //   return expiry > 2022;
+  // }
+  // function func(){
+  //   var filtered=[2022,2021,2025,2023,2021].filter(xdate);
+  //   console.log(filtered);
+  // }
+  // func();
+
+  //reduse
+  // let array1=[150,550,450,600,250];
+  // let ini=0;
+  // let sum=array1.reduce(
+  //   (pre,cur)=>pre+cur,
+  //   ini
+  // );
+  // console.log(sum);
+
+
+  //map Employee
+  // let array =[
+  //   {name: "amit", age: 35, salary: 40000, bonus: 1000, status:true},
+  //   {name: "ajay", age: 25, salary: 38000, bonus: 2000, status:false},
+  //   {name: "mayur", age: 23, salary: 50000, bonus: 500, status:true},
+  //   {name: "jay", age: 29, salary: 35000, bonus: 600, status:true},
+  //   {name: "raj",  age: 33, salary: 22000, bonus: 2000, status:true}
+  // ];
+  // let map=array.map(x => console.log(x.name, x.age, x.salary, x.bonus, x.status));
+
+  //filter Employee
+  // function xdate(salary){
+  //   return salary > 5000;
+  // }
+  // function func(){
+  //   var filtered=[4000,38000,5000,35000,22000].filter(xdate);
+  //   console.log(filtered);
+  // }
+  // func();
+
+  //reduse Employee
+  // let array1=[4000,38000,5000,35000,22000];//salary
+  let array1=[1000,2000,500,600,2000];//bonus
+  let ini=0;
+  let sum=array1.reduce(
+    (pre,cur)=>pre+cur,
+    ini
+  );
+  console.log(sum);
+
 
   return (
     <div className="App">
