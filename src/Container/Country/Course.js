c715a57379f8fbc5eefd1cdedb063784c1cf12f7
binalbodar <binalbodar23@gmail.com>
2022-05-09 19:24:54 +0530

    9_5_22

---
+++ src/Container/Country/Course.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Course({Changecou}) {
+    return (
+        <div>
+            {
+                // Changecou === 'RNW1' ?<p>Course Name:Android</p>:<p>Course Name:Node Js</p>
+            }
+        </div>
+    );
+}
+
+export default Course;
